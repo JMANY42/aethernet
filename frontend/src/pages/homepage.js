6bb75cbcac9f04c4dd7dbef457d64f9efde124d6
tramanhtrinh <trinhfoil@gmail.com>
2025-11-08 17:50:55 -0600

    homepage base

---
+++ frontend/src/pages/homepage.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/Navbar"
+export default function Home() {
+    return (
+        <>
+        <NavBar />
+        <div className="background-home">
+            <img src="mmHomepage.png" alt="Description" class="scaled-image"></img>
+            <button class="login-btn" onClick={() => window.location.href = '/signup'}>Get Started!</button>    
+            <link rel="stylesheet" href="styles.css"></link>
+            </div>
+        </>
+        
+    )
+}
